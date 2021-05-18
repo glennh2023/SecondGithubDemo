73c44cac937b5419273f25b4901f5be318b8f2b7
glennh2023 <glenn.hadcock@gmail.com>
2021-05-18 09:04:16 -0500

    project and java file
    asd

---
+++ src/secondGithubDemo/TuesdayPractice.java
@@ -0,0 +1,11 @@
+package secondGithubDemo;
+
+public class TuesdayPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 5;
+		int num2 = 3;
+	}
+
+}
